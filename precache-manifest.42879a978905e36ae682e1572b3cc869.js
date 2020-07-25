43bb66560f657978dd86693d55ff0733ae7324f7
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-07-25 16:35:15 +0100

    Updates

---
+++ precache-manifest.42879a978905e36ae682e1572b3cc869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78659a8e5e51947b8d4a9b407e3ebe20",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc5759bddf73d06453ba",
+    "url": "/monsters-rolodex/static/css/main.98a4977e.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc5759bddf73d06453ba",
+    "url": "/monsters-rolodex/static/js/main.81a70d64.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
